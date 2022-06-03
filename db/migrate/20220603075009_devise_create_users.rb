ac6f3b5606a22dbed48e051f816d10017538fe94
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-03 17:06:49 +0900

    マイグレーション

--- db/migrate/20220603075009_devise_create_users.rb
+++ db/migrate/20220603075009_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.text :profile,              null: false
+      t.string :time,               null: false
+      t.date :birth_day,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
