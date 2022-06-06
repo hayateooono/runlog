1021cc6a7f79fac3a2cfd679270667e653c0314e
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-06 15:00:51 +0900

    バリデーション

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,11 @@
 class Log < ApplicationRecord
 
+  validates :content, presence: true
+  validates :day,     presence: true
+  validates :distance,presence: true
+  validates :time,    presence: true
+  
+
   belongs_to :user
   has_one_attached :image
 end
