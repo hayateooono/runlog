78730dedab9e08413e7e07872e463a9f01007291
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-06 14:48:09 +0900

    logモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_035848) do
+ActiveRecord::Schema.define(version: 2022_06_06_024050) do
+
+  create_table "logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.date "day", null: false
+    t.integer "distance", null: false
+    t.integer "time", null: false
+    t.string "prace"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_logs_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_06_05_035848) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "logs", "users"
 end
