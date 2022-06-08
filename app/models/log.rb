d60158d9dfd65939043f536db7f00d41002c64b7
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-08 10:57:40 +0900

    バリデーション修正

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,10 +1,10 @@
 class Log < ApplicationRecord
 
-  validates :content, presence: true
+  validates :content, presence: true, length: {maximum: 1000 }
   validates :day,     presence: true
-  validates :distance,presence: true
+  validates :distance,presence: true,numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
   validates :time,    presence: true
-  
+  validates :place, length: { maximum: 50 }
 
   belongs_to :user
   has_one_attached :image
