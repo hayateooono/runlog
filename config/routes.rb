d0b740f95f30da42ac44a0af836bd5e152f71f4c
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-15 16:52:09 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "logs#index"
   resources :logs, only: [:new,:create,:show,:destroy,:edit,:update]
+  resources :users, only: :show
 end
