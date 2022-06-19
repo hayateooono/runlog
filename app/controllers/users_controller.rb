e97e715eb845ebb52a1e27e8101a0ad64ebe8f00
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-19 15:18:19 +0900

    コントローラー設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @logs = user.logs.order("created_at DESC")
+    @user = User.find(params[:id])
+    @logs = @user.logs.order("created_at DESC")
     
   end
 end
