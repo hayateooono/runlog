bfb2f32ba3de3e5b019527675d1796437a1524fd
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-20 15:41:09 +0900

    deviseコントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:profile,:birth_day,:hour_time,:min_time,:sec_time])
+    devise_parameter_sanitizer.permit(:account_update,keys: [:nickname,:profile,:birth_day,:hour_time,:min_time,:sec_time])
   end
 end
