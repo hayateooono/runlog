e7a9a713ee38c58b7e7360c8c6d41c372266fb4b
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-21 15:17:13 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: "users/sesssions",
+    sessions: "users/sessions",
     registrations: "users/registrations"
   }
   
   root to: "logs#index"
   resources :logs, only: [:new,:create,:show,:destroy,:edit,:update]
-  resources :users, only: [:show,:edit]
+  resources :users, only: [:show,:edit,:destroy]
 end
