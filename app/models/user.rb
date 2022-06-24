26a1ba8c57b13393218904d0133102eb66351a1c
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-06-24 12:43:29 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true,length: { maximum: 50 }
          validates :profile, presence: true,length: { maximum: 160 }
-         validates :hour_time, numericality: { greater_than_or_equal_to: 01, less_than_or_equal_to: 20 ,message:"は半角数字で正しく入力してください"},
+         validates :hour_time, numericality: { greater_than_or_equal_to: 01, less_than_or_equal_to: 23 ,message:"は半角数字で正しく入力してください"},
          format: { with: /\A[0-9]+\z/  }
          validates :min_time, numericality: { greater_than_or_equal_to: 00, less_than_or_equal_to: 59 ,message:"は半角数字で正しく入力してください"},
          format: { with: /\A[0-9]+\z/ }
