697b60c56e80fc6f06b45f0712485bf2c7e92c2a
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-07-08 15:17:59 +0900

    マイページ見た目の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @logs = @user.logs.order("created_at DESC")
-    @log = Log.find(params[:id])
+    @logs = @user.logs.order("day DESC")
   end
 end
